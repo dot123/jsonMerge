e67646b3761a7f53f9b2d04817ee459552152aa6
dot123 <conjurer@gmail.com>
2020-04-02 10:44:43 +0800

    initial

---
+++ go.mod
@@ -0,0 +1,7 @@
+module jsonMerge
+
+go 1.13
+
+require (
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ run.bat
@@ -0,0 +1,3 @@
+echo off
+set curPath=%cd% 
+call "./jsonMerge.exe" -input ./input -output ./output/output.json
